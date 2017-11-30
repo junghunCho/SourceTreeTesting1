ac3e35817c209b70b1ce27dc0ca2986381456dc5
JungHun\JungHun_Cho <jhcho@remotesolution.co.kr>
2017-11-30 15:29:19 +0900

    added key scan function

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 
-//source tree testing 17.06.08
+bool Key_Check(unsigned char* cnt, unsigned char* buf)
+{
+	//check key matrix
+
+	//if keys press then return true
+	//else return false
+}
+
+
+
+
+//source tree
 void main(void)
 {
 	unsigned char i;
+	unsigned char cnt;
+	unsigned char buf[4];
 
 	//device init
 	//peripial init
 	//setting interrupt
+
+	if(Key_Check(unsigned char* cnt, unsigned char* buf))
+	{
+
+
+	}
+	else
+		//enter stop mode
 }
 
 
